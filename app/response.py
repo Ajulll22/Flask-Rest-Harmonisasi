88f86a32ed5ed63b2a5881dbe54dda9b7ef997b3
Ajulll22 <ajulrizki@gmail.com>
2022-06-28 23:45:08 +0700

    base response

---
+++ app/response.py
@@ -0,0 +1,27 @@
+from flask import jsonify, make_response
+
+
+def ok(values, message):
+    res = {
+        'values': values,
+        'message': message
+    }
+
+    return make_response(jsonify(res)), 200
+
+
+def badRequest(values, message):
+    res = {
+        'values': values,
+        'message': message
+    }
+
+    return make_response(jsonify(res)), 400
+
+
+def serverError(message):
+    res = {
+        'message': message
+    }
+
+    return make_response(jsonify(res)), 500
